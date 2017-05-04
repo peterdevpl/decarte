8cf9d59853c9826c6b1ce4026fa1af53de72b9cc
Piotr Horzycki <piotr.horzycki@gmail.com>
2017-05-04 23:13:52 +0200

    Refactor: ShopController split into three classes

---
+++ src/AppBundle/Controller/OrderController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class OrderController extends Controller
+{
+    /**
+     * @Route("/zloz-zamowienie", name="order_details")
+     */
+    public function orderDetailsAction()
+    {
+
+    }
+}
--- src/AppBundle/Controller/ProductsController.php
+++ src/AppBundle/Controller/ProductsController.php
@@ -2,16 +2,11 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Product;
-use AppBundle\Entity\SamplesOrder;
-use AppBundle\Entity\SamplesOrderItem;
-use AppBundle\Form\OrderSamplesType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class ShopController extends Controller
+class ProductsController extends Controller
 {
     /**
      * @Route("/sklep/{type}", name="shop_list_collections", requirements={"type": "[0-9a-z\-]+"})
@@ -91,90 +86,5 @@ class ShopController extends Controller
         return $this->render('shop/viewProduct.html.twig', [
             'product' => $product,
         ]);
-    }
-
-    /**
-     * @Route("/zamow/{id}", name="shop_order", requirements={"id": "\d+"})
-     * @param int $id
-     */
-    public function orderAction($id)
-    {
-
-    }
-
-    /**
-     * @Route("/zamow-probki", name="shop_order_samples")
-     * @param Request $request
-     * @return Response
-     */
-    public function orderSamplesAction(Request $request)
-    {
-        $order = new SamplesOrder();
-        for ($n = 0; $n < $this->getParameter('samples_count'); $n++) {
-            $order->addItem(new SamplesOrderItem());
-        }
-
-        $em = $this->getDoctrine()->getManager();
-        $productType = $em->getRepository('AppBundle:ProductType')->find(1);
-        $products = $em->getRepository('AppBundle:Product')->findDemos($productType);
-
-        $form = $this->createForm(OrderSamplesType::class, $order, [
-            'products' => $products,
-        ]);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $order = $form->getData();
-            $this->sendSamplesOrderEmailToShop($order);
-            $this->sendSamplesOrderEmailToCustomer($order);
-
-            return $this->redirectToRoute('shop_order_samples_confirmation');
-        }
-
-        return $this->render('shop/orderSamples.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/zamow-probki/potwierdzenie", name="shop_order_samples_confirmation")
-     * @return Response
-     */
-    public function orderSamplesConfirmationAction()
-    {
-        return $this->render('shop/orderSamplesConfirmation.html.twig');
-    }
-
-    protected function sendSamplesOrderEmailToShop(SamplesOrder $order)
-    {
-        $message = \Swift_Message::newInstance()
-            ->setSubject('ZAMÓWIENIE WWW - PRÓBKI')
-            ->setTo($this->getParameter('admin_mail'))
-            ->setFrom([$this->getParameter('admin_mail') => $order->getName()])
-            ->setReplyTo($order->getEmail())
-            ->setBody(
-                $this->renderView('shop/mail/samplesOrderShop.html.twig', [
-                    'order' => $order,
-                ]),
-                'text/html'
-            );
-
-        $this->get('mailer')->send($message);
-    }
-
-    protected function sendSamplesOrderEmailToCustomer(SamplesOrder $order)
-    {
-        $message = \Swift_Message::newInstance()
-            ->setSubject('Zamówienie decarte.com.pl - próbki')
-            ->setTo($order->getEmail())
-            ->setFrom([$this->getParameter('admin_mail') => 'Sklep ślubny decARTe.com.pl'])
-            ->setReplyTo($this->getParameter('admin_mail'))
-            ->setBody(
-                $this->renderView('shop/mail/samplesOrderCustomer.html.twig', [
-                    'order' => $order,
-                ]),
-                'text/html'
-            );
-
-        $this->get('mailer')->send($message);
     }
 }
