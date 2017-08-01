e5531ddff98675bc2cb68c29a369e39c796cea6a
Piotr Horzycki <piotr.horzycki@gmail.com>
2017-08-01 23:41:00 +0200

    Unused imports in CartController

--- src/CartBundle/Controller/CartController.php
+++ src/CartBundle/Controller/CartController.php
@@ -3,12 +3,10 @@
 namespace CartBundle\Controller;
 
 use CartBundle\Entity\CartItem;
-use CartBundle\Repository\CartRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 class CartController extends Controller
 {
