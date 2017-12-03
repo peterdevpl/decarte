f90acef9bcabc3825be125df70b68553d85a0d1d
Piotr Horzycki <piotr.horzycki@gmail.com>
2017-12-03 21:29:50 +0100

    Delete original product images

--- src/ProductBundle/Entity/Event/ProductImageListener.php
+++ src/ProductBundle/Entity/Event/ProductImageListener.php
@@ -7,17 +7,20 @@ use ProductBundle\Entity\ProductImage;
 
 class ProductImageListener
 {
+    protected $originalDirectory;
     protected $bigDirectory;
     protected $smallDirectory;
 
-    public function __construct(string $bigDirectory, string $smallDirectory)
+    public function __construct(string $originalDirectory, string $bigDirectory, string $smallDirectory)
     {
+        $this->originalDirectory = $originalDirectory;
         $this->bigDirectory = $bigDirectory;
         $this->smallDirectory = $smallDirectory;
     }
 
     public function preRemove(ProductImage $object, LifecycleEventArgs $args)
     {
+        @unlink($this->originalDirectory . '/' . $object->getOriginalName());
         @unlink($this->bigDirectory . '/' . $object->getBigName());
         @unlink($this->smallDirectory . '/' . $object->getSmallName());
     }
