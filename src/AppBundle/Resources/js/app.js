b8606edd2aff6a4fc03b4311423a575db1077a75
Piotr Horzycki <piotr.horzycki@gmail.com>
2017-12-28 21:53:57 +0100

    Improved product details view

--- src/AppBundle/Resources/js/app.js
+++ src/AppBundle/Resources/js/app.js
@@ -1,1 +1,16 @@
 require('../scss/main.scss');
+
+var $sliderImages = $('#slider img');
+if ($sliderImages.length > 1) {
+    $thumbnails = $('<ul id="slider-thumbnails" />');
+    $sliderImages.each(function(index) {
+        $thumbnails.append('<li><img src="' + $(this).attr('src') + '" data-index="' + (index + 1) + '"></li>');
+    });
+    $('#slider').after($thumbnails);
+
+    $('#slider-thumbnails img').click(function() {
+        var index = $(this).data('index');
+        $('#slider img:not(:nth-child(' + index + '))').css('opacity', 0);
+        $('#slider img:nth-child(' + index + ')').css('opacity', 1);
+    })
+}
