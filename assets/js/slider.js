ebc0b79748e88b17d354984aa73d6ed531bd7b8c
Piotr Horzycki <piotr.horzycki@gmail.com>
2018-12-26 22:47:37 +0100

    Slider script extracted from app.js
    This allows better code organization for future scripts

---
+++ assets/js/slider.js
@@ -0,0 +1,25 @@
+let sliderImages = document.querySelectorAll('#slider img');
+if (sliderImages.length > 1) {
+    let thumbnails = document.createElement('ul');
+    thumbnails.id = 'slider-thumbnails';
+    sliderImages.forEach(function (image, index) {
+        let listItem = document.createElement('li');
+        let thumbnail = document.createElement('img');
+        thumbnail.src = image.src;
+        thumbnail.setAttribute('data-index', (index + 1).toString());
+        thumbnail.addEventListener('click', function() {
+            let index = this.getAttribute('data-index');
+            let imagesOff = document.querySelectorAll('#slider img:not(:nth-child(' + index + '))');
+            let imageOn = document.querySelector('#slider img:nth-child(' + index + ')');
+            imagesOff.forEach(function(image) {
+                image.style.opacity = 0;
+            });
+            imageOn.style.opacity = 1;
+        });
+        listItem.appendChild(thumbnail);
+        thumbnails.appendChild(listItem);
+    });
+
+    let slider = document.getElementById('slider');
+    slider.parentNode.insertBefore(thumbnails, slider.nextSibling);
+}
