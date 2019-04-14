16d17a2663e9b7a5f187a43f4d4818e4ab182e71
Piotr Horzycki <piotr.horzycki@gmail.com>
2019-04-14 18:43:32 +0200

    Simple test of browsing a products group

--- features/browsing_products.feature
+++ features/browsing_products.feature
@@ -6,3 +6,8 @@ Feature:
   Scenario: The home page shows basic informations
     When I am on the homepage
     Then I should see the company logo
+
+  Scenario: Browsing collections of products
+    When I open "zaproszenia-slubne" products
+    Then I should see 1 collection
+    And that collection's name should be "Mystic Moments"
--- src/Behat/Context/FeatureContext.php
+++ src/Behat/Context/FeatureContext.php
@@ -16,4 +16,28 @@ final class FeatureContext extends MinkContext implements Context
     {
         $this->assertElementOnPage('header .site-logo a img');
     }
+
+    /**
+     * @When I open :type products
+     */
+    public function iOpenProductsType(string $type): void
+    {
+        $this->visit('/sklep/' . $type);
+    }
+
+    /**
+     * @Then I should see :count collection(s)
+     */
+    public function iShouldSeeCollections(int $count): void
+    {
+        $this->assertNumElements($count, '#collections li');
+    }
+
+    /**
+     * @Then that collection's name should be :name
+     */
+    public function collectionNameShouldBe(string $name): void
+    {
+        $this->assertElementContainsText('#collections li:first-child figure figcaption h2', $name);
+    }
 }
