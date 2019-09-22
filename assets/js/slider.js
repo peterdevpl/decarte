a9ee453431bf534c5d7a61c49cf76e5c5b27afe1
Piotr Horzycki <piotr.horzycki@gmail.com>
2019-09-22 18:53:21 +0200

    Improved JS: const instead of let/var, updated webpack configuration

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,16 +1,16 @@
-let sliderImages = document.querySelectorAll('#slider img');
+const sliderImages = document.querySelectorAll('#slider img');
 if (sliderImages.length > 1) {
-    let thumbnails = document.createElement('ul');
+    const thumbnails = document.createElement('ul');
     thumbnails.id = 'slider-thumbnails';
     sliderImages.forEach(function (image, index) {
-        let listItem = document.createElement('li');
-        let thumbnail = document.createElement('img');
+        const listItem = document.createElement('li');
+        const thumbnail = document.createElement('img');
         thumbnail.src = image.src;
         thumbnail.setAttribute('data-index', (index + 1).toString());
         thumbnail.addEventListener('click', function() {
-            let index = this.getAttribute('data-index');
-            let imagesOff = document.querySelectorAll('#slider img:not(:nth-child(' + index + '))');
-            let imageOn = document.querySelector('#slider img:nth-child(' + index + ')');
+            const index = this.getAttribute('data-index');
+            const imagesOff = document.querySelectorAll('#slider img:not(:nth-child(' + index + '))');
+            const imageOn = document.querySelector('#slider img:nth-child(' + index + ')');
             imagesOff.forEach(function(image) {
                 image.style.opacity = 0;
             });
@@ -20,6 +20,6 @@ if (sliderImages.length > 1) {
         thumbnails.appendChild(listItem);
     });
 
-    let slider = document.getElementById('slider');
+    const slider = document.getElementById('slider');
     slider.parentNode.insertBefore(thumbnails, slider.nextSibling);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-var Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
 
 Encore
     .setOutputPath('public_html/build/')
     .setPublicPath('/build')
     .addEntry('app', './assets/js/app.js')
     .createSharedEntry('vendor', './assets/scss/bootstrap.scss')
+    .disableSingleRuntimeChunk()
     .enableSassLoader()
     .enableSourceMaps(!Encore.isProduction())
     .cleanupOutputBeforeBuild()
