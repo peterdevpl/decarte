0109422979c15d15e858e348eb4b023f4a54950f
Piotr Horzycki <piotr.horzycki@gmail.com>
2022-02-13 21:42:59 +0100

    Added realization type fixtures

---
+++ src/DataFixtures/RealizationTypeFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Decarte\Shop\DataFixtures;
+
+use Decarte\Shop\Entity\Order\RealizationType;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+final class RealizationTypeFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $standard = new RealizationType();
+        $standard
+            ->setName('Standard')
+            ->setDTPDays(7)
+            ->setDeliveryDays(14)
+            ->setPrice(0)
+            ->setIsVisible(true);
+        $manager->persist($standard);
+
+        $express = new RealizationType();
+        $express
+            ->setName('Express')
+            ->setDTPDays(7)
+            ->setDeliveryDays(7)
+            ->setPrice(15000)
+            ->setIsVisible(true);
+        $manager->persist($express);
+
+        $manager->flush();
+    }
+}
